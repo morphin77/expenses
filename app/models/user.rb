bbc64f0e1e0e92ea5250ae54507eb2f7f1c2ecc7
Kulakov Oleg <olegkulakov77@gmail.com>
2017-08-20 01:16:08 +0300

    fix trouble with brunches again

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-  
-=======
->>>>>>> 7f9db3b2c2e2b6c7aafd5c09cce8861e556e1308
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
